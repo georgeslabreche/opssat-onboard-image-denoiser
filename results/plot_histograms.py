9f5f7244f05992fe0892cf4f2ad6eb17ac9dd8b3
Georges Labrèche <georges@tanagraspace.com>
2023-09-24 18:01:12 -0400

    issue #43 resize original image and include option to normalize pixel counts

--- results/plot_histograms.py
+++ results/plot_histograms.py
@@ -6,10 +6,15 @@ import numpy as np
 # FIXME: The subplots for the original and denoised RGB histograms do not have the same x- and y- axis scale.
 # Let's just manually fix this for the figures we end up including for publication.
 
-def plot_histograms_rgb(image_timestamp, image_path_original, image_path_denoised, save_fig=True):
+def plot_histograms_rgb(image_timestamp, original_image_resize_target, image_path_original, image_path_denoised, normalize_counts, save_fig):
+
   # Load the images using PIL
   image_original = Image.open(image_path_original)
   image_denoised = Image.open(image_path_denoised)
+
+  if original_image_resize_target is not None:
+    new_dimensions  = (original_image_resize_target, original_image_resize_target)
+    image_original = image_original.resize(new_dimensions)
 
   # Convert the images into arrays
   data_original = np.array(image_original)
@@ -26,15 +31,18 @@ def plot_histograms_rgb(image_timestamp, image_path_original, image_path_denoise
     green_channel = data[:, :, 1]
     blue_channel = data[:, :, 2]
 
-    # Plotting the Red, Green, and Blue histograms with normalized counts
-    plt.hist(red_channel.ravel(), bins=256, color='red', alpha=0.5, rwidth=0.8, density=True, label='Red Channel')
-    plt.hist(green_channel.ravel(), bins=256, color='green', alpha=0.5, rwidth=0.8, density=True, label='Green Channel')
-    plt.hist(blue_channel.ravel(), bins=256, color='blue', alpha=0.5, rwidth=0.8, density=True, label='Blue Channel')
+    # Plotting the Red, Green, and Blue histograms
+    plt.hist(red_channel.ravel(), bins=256, color='red', alpha=0.5, rwidth=0.8, density=normalize_counts, label='Red Channel')
+    plt.hist(green_channel.ravel(), bins=256, color='green', alpha=0.5, rwidth=0.8, density=normalize_counts, label='Green Channel')
+    plt.hist(blue_channel.ravel(), bins=256, color='blue', alpha=0.5, rwidth=0.8, density=normalize_counts, label='Blue Channel')
 
     title = 'Original RGB Histograms' if idx == 1 else 'Denoised RGB Histograms'
     plt.title(title)
     plt.xlabel('Pixel Intensity')
-    plt.ylabel('Probability Density')
+    if normalize_counts:
+      plt.ylabel('Probability Density')
+    else:
+      plt.ylabel('Counts')
     plt.grid(axis='y', alpha=0.75)
     plt.legend(loc='upper right')
 
@@ -45,19 +53,25 @@ def plot_histograms_rgb(image_timestamp, image_path_original, image_path_denoise
     plt.savefig(f"figures/image-histograms/{image_timestamp}.rgb.svg", format="svg")
   else:
     plt.show()
- 
-def plot_histograms_grayscale(image_timestamp, image_path_original, image_path_denoised, save_fig=True):
+
+
+def plot_histograms_grayscale(image_timestamp, original_image_resize_target, image_path_original, image_path_denoised, normalize_counts, save_fig):
   # Load the images using OpenCV
   image_original = cv2.imread(image_path_original, cv2.IMREAD_GRAYSCALE)
   image_denoised = cv2.imread(image_path_denoised, cv2.IMREAD_GRAYSCALE)
+  
+  if original_image_resize_target is not None:
+    new_dimensions  = (original_image_resize_target, original_image_resize_target)
+    image_original = cv2.resize(image_original, new_dimensions)
 
   # Compute histograms
   hist_original = cv2.calcHist([image_original], [0], None, [256], [0, 256])
   hist_denoised = cv2.calcHist([image_denoised], [0], None, [256], [0, 256])
 
   # Normalize histograms for better visualization
-  hist_original /= hist_original.sum()
-  hist_denoised /= hist_denoised.sum()
+  if normalize_counts is True:
+    hist_original /= hist_original.sum()
+    hist_denoised /= hist_denoised.sum()
 
   # Get non-zero ranges
   nz_original = np.where(hist_original > 0)
@@ -87,7 +101,18 @@ def plot_histograms_grayscale(image_timestamp, image_path_original, image_path_d
   else:
     plt.show()
 
+
+# The original images are the default thumbnails retrieved from the spacecraft
+# This thumbnail is resized prior to onboard processing
+# We don't retrieve the resized version from the spacecraft so we have to resize it here
+original_image_resize_target = 224
+
+# Save the generated plots as SVG files
 save_fig = True
+
+# Normalize the pixel count
+normalize_counts = False
+
 ml_method = "AE"
 noise_type = "FNP"
 noise_factor = "50"
@@ -98,5 +123,5 @@ image_path_original = f"images/{ml_method}/{noise_type}-{noise_factor}/Earth/{im
 image_path_denoised = f"images/{ml_method}/{noise_type}-{noise_factor}/Earth/{image_timestamp}.denoised.jpeg"
 
 # Plot histograms
-plot_histograms_rgb(image_timestamp, image_path_original, image_path_denoised, save_fig)
-plot_histograms_grayscale(image_timestamp, image_path_original, image_path_denoised, save_fig)
+plot_histograms_rgb(image_timestamp, original_image_resize_target, image_path_original, image_path_denoised, normalize_counts, save_fig)
+plot_histograms_grayscale(image_timestamp, original_image_resize_target, image_path_original, image_path_denoised, normalize_counts, save_fig)
