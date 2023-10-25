b3b36daca54cc4481170b4e9a983f614deb019d4
Dr. Cesar Guzman <cguzman@telespazio.de>
2023-10-25 06:06:38 +0200

    Update readme and separate input parameters for script

--- results/flatsat/calculate_metrics.py
+++ results/flatsat/calculate_metrics.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import argparse
 import pandas as pd
 from skimage.metrics import *
 from skimage import io, transform
@@ -15,50 +16,60 @@ def compute_psnr(img1, img2):
 def compute_ssim(img1, img2):
   return structural_similarity(img1, img2, multichannel=True, data_range=255, win_size=3)
 
-# Load the reference image
-reference_image_path = "wgan_fpn50_p0-8_01/images/sample.jpeg"
-reference_image = io.imread(reference_image_path)
-reference_image = transform.resize(reference_image, (224, 224), anti_aliasing=True)
-reference_image = (reference_image * 255).astype('uint8')  # Convert to uint8
+def calculate_metric(reference_folder_path, noise):
 
-# Read the CSV file
-csv_path = "wgan_fpn50_p0-8_01/metrics.csv"
-df = pd.read_csv(csv_path)
+  # Load the reference image
+  reference_image_path = f"{reference_folder_path}/images/sample.jpeg"
+  reference_image = io.imread(reference_image_path)
+  reference_image = transform.resize(reference_image, (224, 224), anti_aliasing=True)
+  reference_image = (reference_image * 255).astype('uint8')  # Convert to uint8
 
-# Calculate metrics for each image and update the CSV
-for index, row in df.iterrows():
-  patch_margin_pixels = int(row['patch_margin_pixels'])
+  # Read the CSV file
+  csv_path = f"{reference_folder_path}/metrics.csv"
+  df = pd.read_csv(csv_path)
 
-  # Derive the denoised image name based on the patch_margin_pixels column
-  denoised_image_name = f"sample.fpn50.p{patch_margin_pixels}.denoised.jpeg"
-  denoised_image_path = os.path.join("wgan_fpn50_p0-8_01/images", denoised_image_name)
-  
-  # Read the denoised image
-  denoised_image = io.imread(denoised_image_path)
+  # Calculate metrics for each image and update the CSV
+  for index, row in df.iterrows():
+    patch_margin_pixels = int(row['patch_margin_pixels'])
 
-  # Compute metrics
-  mse = compute_mse(reference_image, denoised_image)
-  psnr = compute_psnr(reference_image, denoised_image)
-  ssim = compute_ssim(reference_image, denoised_image)
+    # Derive the denoised image name based on the patch_margin_pixels column
+    denoised_image_name = f"sample.{noise}.p{patch_margin_pixels}.denoised.jpeg"
+    denoised_image_path = os.path.join(f"{reference_folder_path}/images", denoised_image_name)
+    
+    # Read the denoised image
+    denoised_image = io.imread(denoised_image_path)
 
-  # Update the CSV
-  df.at[index, 'mse'] = mse
-  df.at[index, 'psnr'] = psnr
-  df.at[index, 'ssim'] = ssim
+    # Compute metrics
+    mse = compute_mse(reference_image, denoised_image)
+    psnr = compute_psnr(reference_image, denoised_image)
+    ssim = compute_ssim(reference_image, denoised_image)
 
-# Save the updated CSV
-df.to_csv(csv_path, index=False)
+    # Update the CSV
+    df.at[index, 'mse'] = mse
+    df.at[index, 'psnr'] = psnr
+    df.at[index, 'ssim'] = ssim
 
-# Pretty print the updated CSV
-print(df)
+  # Save the updated CSV
+  df.to_csv(csv_path, index=False)
 
-# Round the values in the dataframe
-columns_to_round = ['mse', 'psnr', 'ssim']
-df[columns_to_round] = df[columns_to_round].round(3)
+  # Pretty print the updated CSV
+  print(df)
 
-# Save the rounded CSV
-rounded_csv_path = csv_path.replace(".csv", ".rounded.csv")
-df.to_csv(rounded_csv_path, index=False)
+  # Round the values in the dataframe
+  columns_to_round = ['mse', 'psnr', 'ssim']
+  df[columns_to_round] = df[columns_to_round].round(3)
 
-# Pretty print the rounded CSV
-print(df)
+  # Save the rounded CSV
+  rounded_csv_path = csv_path.replace(".csv", ".rounded.csv")
+  df.to_csv(rounded_csv_path, index=False)
+
+  # Pretty print the rounded CSV
+  print(df)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--reference_folder_path', required=True, type=str, help='Path to the reference folder')
+  parser.add_argument('--noise', required=True, type=str, help='The type of noise of the results')
+  args = parser.parse_args()
+
+  reference_image = calculate_metric(args.reference_folder_path, args.noise)
--- results/flatsat/plot_histograms.py
+++ results/flatsat/plot_histograms.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import cv2
 import pandas as pd
 import numpy as np
@@ -34,64 +35,78 @@ def plot_rgb_histogram(image, xlim=(0, 256), save_path=None):
     plt.show()
 
 
+def plot_histogram(reference_folder_path, noise_image, output_three_channels, output_final):
 
-# Load and resize the reference image
-reference_image_path = "wgan_fpn50_p0-8_01/images/sample.jpeg"
-reference_image = cv2.imread(reference_image_path)
-reference_image = cv2.resize(reference_image, (224, 224))
+  # Load and resize the reference image
+  reference_image_path = f"{reference_folder_path}/images/sample.jpeg"
+  reference_image = cv2.imread(reference_image_path)
+  reference_image = cv2.resize(reference_image, (224, 224))
 
-# Plot RGB histograms for the original image
-plot_rgb_histogram(reference_image, xlim=(20, 120), save_path="wgan_fpn50_p0-8_01/figures/histogram_rgb_original.svg")
-
-
-# Create another figure for the histograms of the WGAN p6 denoised image
-plt.figure(figsize=(10, 9))
-denoised_image_name = f"sample.fpn50.p6.denoised.jpeg"
-denoised_image_path = os.path.join("wgan_fpn50_p0-8_01/images", denoised_image_name)
-denoised_image = cv2.imread(denoised_image_path)
-
-# PLot RGB histograms
-plot_rgb_histogram(denoised_image, xlim=(20, 120), save_path="wgan_fpn50_p0-8_01/figures/histogram_rgb_denoised_wgan_p6.svg")
+  # Plot RGB histograms for the original image
+  plot_rgb_histogram(reference_image, xlim=(20, 120), save_path=f"{reference_folder_path}/figures/histogram_rgb_original.svg")
 
 
+  # Create another figure for the histograms of the WGAN p6 denoised image
+  plt.figure(figsize=(10, 9))
+  denoised_image_name = f"sample.{noise_image}.denoised.jpeg"
+  denoised_image_path = os.path.join(f"{reference_folder_path}/images/", denoised_image_name)
+  denoised_image = cv2.imread(denoised_image_path)
 
-
-# Plot the grayscale historgrams of original image + denoised
-plt.figure(figsize=(10, 4))
-
-# Define colors using the RdYlBu palette from palettable
-# FIXME: Use line types instead + legend
-
-# Plot grayscale histogram for the original image
-reference_image_gray = cv2.imread(reference_image_path, cv2.IMREAD_GRAYSCALE)
-reference_image_gray = cv2.resize(reference_image_gray, (224, 224))
-plot_grayscale_histogram(reference_image_gray, "Original image", xlim=(20, 120), fill=False)
-
-denoised_image_name = "sample.fpn50.p6.denoised.jpeg"
-denoised_image_path = os.path.join("wgan_fpn50_p0-8_01/images", denoised_image_name)
-denoised_image = cv2.imread(denoised_image_path, cv2.IMREAD_GRAYSCALE)
-denoised_image = cv2.resize(denoised_image, (224, 224))
-plot_grayscale_histogram(denoised_image, f'WGAN Denoised (margin is 6 pixels)', xlim=(20, 120), fill=False)
+  # PLot RGB histograms
+  temp = f"{reference_folder_path}/figures/{output_three_channels}"
+  print (temp)
+  plot_rgb_histogram(denoised_image, xlim=(20, 120), save_path=temp)
 
 
 
-'''
-# Create another figure with grayscale histograms of all denoised images
-# Read the CSV file
-csv_path = "wgan_fpn50_p0-8_01/metrics.csv"
-df = pd.read_csv(csv_path)
 
-# For each patch_margin_pixels value in the CSV, plot the grayscale histogram for the corresponding denoised image
-for index, patch_margin_pixels in enumerate(df['patch_margin_pixels']):
-  denoised_image_name = f"sample.fpn50.p{int(patch_margin_pixels)}.denoised.jpeg"
-  denoised_image_path = os.path.join("wgan_fpn50_p0-8_01/images", denoised_image_name)
-  denoised_image = cv2.imread(denoised_i
-  plot_grayscale_histogram(denoised_image, f'p{int(patch_margin_pixels)}', xlim=(20, 120), fill=False)mage_path, cv2.IMREAD_GRAYSCALE)
+  # Plot the grayscale historgrams of original image + denoised
+  plt.figure(figsize=(10, 4))
+
+  # Define colors using the RdYlBu palette from palettable
+  # FIXME: Use line types instead + legend
+
+  # Plot grayscale histogram for the original image
+  reference_image_gray = cv2.imread(reference_image_path, cv2.IMREAD_GRAYSCALE)
+  reference_image_gray = cv2.resize(reference_image_gray, (224, 224))
+  plot_grayscale_histogram(reference_image_gray, "Original image", xlim=(20, 120), fill=False)
+
+  denoised_image_name = f"sample.{noise_image}.denoised.jpeg"
+  denoised_image_path = os.path.join(f"{reference_folder_path}/images", denoised_image_name)
+  denoised_image = cv2.imread(denoised_image_path, cv2.IMREAD_GRAYSCALE)
   denoised_image = cv2.resize(denoised_image, (224, 224))
-'''
+  plot_grayscale_histogram(denoised_image, f'WGAN Denoised (margin is 6 pixels)', xlim=(20, 120), fill=False)
 
-plt.xlabel('Pixel Intensity')
-plt.ylabel('Frequency')
-plt.legend()
-plt.savefig("wgan_fpn50_p0-8_01/figures/histogram_grayscale_original_and_denoised_wgan_p0-8.svg", format="svg")
-plt.show()
+
+
+  '''
+  # Create another figure with grayscale histograms of all denoised images
+  # Read the CSV file
+  csv_path = f"{reference_folder_path}/metrics.csv"
+  df = pd.read_csv(csv_path)
+
+  # For each patch_margin_pixels value in the CSV, plot the grayscale histogram for the corresponding denoised image
+  for index, patch_margin_pixels in enumerate(df['patch_margin_pixels']):
+    denoised_image_name = f"sample.fpn50.p{int(patch_margin_pixels)}.denoised.jpeg"
+    denoised_image_path = os.path.join(f"{reference_folder_path}/images", denoised_image_name)
+    denoised_image = cv2.imread(denoised_i
+    plot_grayscale_histogram(denoised_image, f'p{int(patch_margin_pixels)}', xlim=(20, 120), fill=False)mage_path, cv2.IMREAD_GRAYSCALE)
+    denoised_image = cv2.resize(denoised_image, (224, 224))
+  '''
+
+  plt.xlabel('Pixel Intensity')
+  plt.ylabel('Frequency')
+  plt.legend()
+  plt.savefig(f"{reference_folder_path}/figures/{output_final}", format="svg")
+  plt.show()
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--reference_folder_path', required=True, type=str, help='Path to the reference folder')
+  parser.add_argument('--noise_image', required=True, type=str, help='The type of noise and image of the results')
+  parser.add_argument('--output_three_channels', required=True, type=str, help='The name of the output for three channels')
+  parser.add_argument('--output_final', required=True, type=str, help='The final name of the file')
+  args = parser.parse_args()
+
+  reference_image = plot_histogram(args.reference_folder_path, args.noise_image, args.output_three_channels, args.output_final)
